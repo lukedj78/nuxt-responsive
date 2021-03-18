6740ec245e8a2af5ab6c0842638b1b3ddee1f294
Luca Di Gerlando <lucadigerlando@gmail.com>
2021-03-18 16:13:31 +0100

    feat: fix

--- components/BaseMenuItem.vue
+++ components/BaseMenuItem.vue
@@ -1,12 +1,20 @@
 <template>
-  <div v-click-outside="closeEvent" class="relative" @mouseover="show">
+  <div
+    v-click-outside="closeEvent"
+    class="relative"
+    @mouseover="showSubMenu = true"
+  >
     <NuxtLink :to="item.to">{{ item.name }}</NuxtLink>
     <ul
       v-if="item.menu && showSubMenu"
       class="mt-5 p-4 text-gray-500 bg-white absolute w-48 rounded space-y-3"
     >
-      <li v-for="(subItem, i) in item.menu" :key="`sub-item-${i}`">
-        <NuxtLink v-if="subItem.name" :to="subItem.to" @click.stop="hide">{{
+      <li
+        v-for="(subItem, i) in item.menu"
+        :key="`sub-item-${i}`"
+        @click.stop="showSubMenu = false"
+      >
+        <NuxtLink v-if="subItem.name" :to="subItem.to">{{
           subItem.name
         }}</NuxtLink>
         <hr v-if="subItem.isDivider" />
@@ -39,6 +47,9 @@ export default {
     },
     closeEvent() {
       this.hide()
+    },
+    navigateTo(to) {
+      this.$router.push(to)
     },
   },
 }
